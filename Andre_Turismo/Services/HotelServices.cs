c9bab8208204a27333d226070b5270339907f88b
misuracandre <m1sur4k@gmail.com>
2023-04-18 01:11:30 -0300

    clientServices criada

--- Andre_Turismo/Services/HotelServices.cs
+++ Andre_Turismo/Services/HotelServices.cs
@@ -3,10 +3,64 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Andre_Turismo.Models;
+using Microsoft.Data.SqlClient;
 
 namespace Andre_Turismo.Services
 {
-    internal class HotelServices
+    public class HotelServices
     {
+        readonly string strConn = @"Server=(localdb)\MSSQLLocalDB;Integrated Security=true;AttachDbFileName=C:\Users\adm\Documents\fly.mdf;";
+        readonly SqlConnection conn;
+
+        public HotelServices()
+        {
+            conn = new SqlConnection(strConn);
+            conn.Open();
+        }
+
+        public bool Insert(Hotel hotel)
+        {
+            bool status = false;
+
+            try
+            {
+                string strInsert = "insert into Hotel (Name, IdAddress)" + "values (@Name, @IdAddress)";
+
+                SqlCommand commandInsert = new SqlCommand(strInsert, conn);
+
+                commandInsert.Parameters.Add(new SqlParameter("@Name", hotel.Name));
+                commandInsert.Parameters.Add(new SqlParameter("@IdAddress", InsertAddress(hotel)));
+
+                commandInsert.ExecuteNonQuery();
+                status = true;
+            }
+            catch (Exception)
+            {
+                status = false;
+                throw; 
+            }
+            finally
+            {
+                conn.Close();
+            }
+            return status;
+        }
+
+        public int InsertAddress(Hotel hotel)
+        {
+            string strInsert = "insert into Hotel (Street, Number, Neighborhood, ZipCode, Extension)" + "values (@Street, @Number, @Neighborhood, @ZipCode, @Extension); select cast(scope_identity() as int";
+
+            SqlCommand commandInsert = new SqlCommand(strInsert, conn);
+            commandInsert.Parameters.Add(new SqlParameter("@Street", hotel.Address.Street));
+            commandInsert.Parameters.Add(new SqlParameter("@Number", hotel.Address.Number));
+            commandInsert.Parameters.Add(new SqlParameter("@Neighborhood", hotel.Address.Neighborhood));
+            commandInsert.Parameters.Add(new SqlParameter("@ZipCode", hotel.Address.ZipCode));
+            commandInsert.Parameters.Add(new SqlParameter("@Extension", hotel.Address.Extension));
+
+            return (int)commandInsert.ExecuteScalar();
+
+
+        }
     }
 }
