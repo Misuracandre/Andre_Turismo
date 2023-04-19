6882c7295c5f561a678c47763517455a7028cffd
misuracandre <m1sur4k@gmail.com>
2023-04-19 02:48:20 -0300

    package com dapper

--- Andre_Turismo/Models/Package.cs
+++ Andre_Turismo/Models/Package.cs
@@ -8,11 +8,19 @@ namespace Andre_Turismo.Models
 {
     public class Package
     {
+        public readonly static string INSERT = "insert into Package (IdHotel, IdClient, IdTicket, Value)" + "values (@IdHotel, @IdClient, @IdTicket, @Value)";
+
+        public readonly static string GETALL = "select IdHotel, IdClient, IdTicket, Value from Package";
         public int Id { get; set; }
         public Hotel Hotel { get; set; }
         public Ticket Ticket { get; set; }
         public decimal Value { get; set; }
         public Client Client { get; set; }
         //public DateTime Registration_Date { get; set; }
+
+        public override string ToString()
+        {
+            return Newtonsoft.Json.JsonConvert.SerializeObject(this);
+        }
     }
 }
